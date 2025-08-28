75ddc55dc64de5ffcbba90e4b5a23b4c44f1d4b3
Vinicius de Souza <vinicius.souza110208@gmail.com>
2025-08-28 16:25:37 -0300

    Add files via upload

--- Web_App/configuracao_user.php
+++ Web_App/configuracao_user.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +18,9 @@
 <body style="background-color: lightgray;">
     <ul class="nav justify-content-end" style="background-color: white;">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#" style="color: black;">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#" style="color: black;">Link</a>
+    <a class="nav-link" href="index.php" style="color: black;">
+        <i class="bi bi-person-circle"></i>
+    </a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="configuracao_user.php">
@@ -42,6 +45,12 @@
                         <a href="edit_cadastro.php">
                             <button type="button" class="btn btn-secondary">
                                 Change Password
+                            </button>
+                        </a>
+                        <br><br>
+                        <a href="delete_account.php">
+                            <button type="button" class="btn btn-secondary" id="delete_account" name = 'delete_account'>
+                                Delete Account
                             </button>
                         </a>
                     </div>
