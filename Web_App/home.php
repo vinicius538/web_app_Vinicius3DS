5cc9fd26e29bfa3736e2e7238d03e23ffdad06f5
Vinicius de Souza <vinicius.souza110208@gmail.com>
2025-09-24 16:27:37 -0300

    Update home.php

--- Web_App/home.php
+++ Web_App/home.php
@@ -59,7 +59,7 @@
                       <div class="card" style="width: 18rem;">
                       <img src="..." class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">Copacabana</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
@@ -69,7 +69,7 @@
                       <div class="card" style="width: 18rem;">
                       <img src="..." class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">Feira de São Cristóvão</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
@@ -79,7 +79,7 @@
                       <div class="card" style="width: 18rem;">
                       <img src="..." class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">Rock in Rio</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
