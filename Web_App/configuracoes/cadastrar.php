3c2a77dae7a6a3c7384067c882623f673a200fa1
Vinicius de Souza <vinicius.souza110208@gmail.com>
2025-10-02 14:26:40 -0300

    Organização + progresso dos eventos
    Adicionei a pagina de criação de evento

---
+++ Web_App/configuracoes/cadastrar.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit'])){
+
+        include_once('configuracoes/config.php');
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $name = $_POST['name'];
+        $number = $_POST['number'];
+        $surname = $_POST['surname'];
+
+        if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM sign_up WHERE email='$email'")) > 0) {
+            echo "<script>alert('Email already registered. Please use a different email.');</script>";
+        } else {
+            $result = mysqli_query($conn, "INSERT INTO sign_up(email, senha, nome, numero) VALUES('$email', '$password', '$name', '$number')");
+            echo "<script>alert('Your account has been created!')</script>";
+        }
+        
+    }
+?>
--- Web_App/create_event.php
+++ Web_App/create_event.php
@@ -1,115 +1,78 @@
+<?php
+    include_once('configuracoes/config.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Create event</title>
 </head>
 <body style="background-color: lightgray;">
-    <ul class="nav justify-content-end" style="background-color: white;">
-  <li class="nav-item">
-    <a class="nav-link" href="index.php" style="color: black;">
-      <i class="bi bi-person-circle"></i>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="configuracao_user.php">
-        <i class="bi bi-gear-fill" style="color: black;"></i>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a href="home.php" class="nav-link">
-        <i class="bi bi-house-door-fill" style="color: black;"></i>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="tab_usuarios.php">
-        <i class="bi bi-table" style="color:black"></i>
-    </a>
-  </li>
-  <li>
-    <img src="img/logo_web_app-removebg-preview.png" style="width:30px ;height:35px ;margin-right:5px ; padding-top:5px ;" alt="">
-  </li>
-</ul>
     <div class="container">
         <div class="row">
-          <div class="col-11 mx-auto" style="background-color: white; margin-top: 30px; padding-bottom: 80px;">
-              <h3 class="poppins-light" style="padding-top: 50px;">
-                  <strong class="d-block text-center">
-                      Welcome to the Web App
-                  </strong>
-              </h3>
-              <table>
-                <thead>
-                  <tr>
-                    <th>
-                      Your events
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <a href="">
-                        <button>
-                          <img src="img/pluh.jpg" style="width: 15rem;" alt="">
-                        </button>
-                      </a>
-                </tbody>
+          <form action="sign_up.php" method="POST">
+              <div class="col-8 mx-auto" style="background-color: white; margin-top: 30px; padding-bottom: 40px;">
+                  <h3 class="poppins-light" style="padding-top: 50px;">
+                      <strong class = 'd-block text-center'>
+                          Create event
+                      </strong>
+                  </h3>
+                  <div class="text-center">
+                    <img src="img/logo_web_app-removebg-preview.png" class="rounded" style="width:50px ;height:50px;" alt="">
+                  </div>
 
-              </table>
-              <table>
-                <thead>
-                  <tr>
-                    <th>
-                      Recent
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <div class="card" style="width: 18rem;">
-                      <img src="..." class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Copacabana</h5>
-                        <p class="card-text">Praia icônica do Rio, famosa pelo calçadão, eventos ao ar livre e o Réveillon que atrai milhões.</p>
-                        <a href="#" class="btn btn-secondary">Go somewhere</a>
+                  <div class="row">
+                    <p>
+                    
+                      <div class="form-floating mb-3" style="width: 300px; margin-left: 70px;">
+                        <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" required>
+                        <label for="floatingInput">Name</label>
                       </div>
-                    </div>
-                    </td>
-                    <td>
-                      <div class="card" style="width: 18rem;">
-                      <img src="..." class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Feira de São Cristóvão</h5>
-                        <p class="card-text">Espaço cultural com comidas típicas, música e artesanato que celebra a cultura nordestina.</p>
-                        <a href="#" class="btn btn-secondary">Go somewhere</a>
+                      <div class="form-floating" style="width: 300px;">
+                        <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password" minlength="4" required>
+                        <label for="floatingPassword">Date</label>
                       </div>
-                    </div>
-                    </td>
-                    <td>
-                      <div class="card" style="width: 18rem;">
-                      <img src="..." class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Rock in Rio</h5>
-                        <p class="card-text">Um dos maiores festivais de música do mundo, com shows nacionais e internacionais incríveis.</p>
-                        <a href="#" class="btn btn-secondary">Go somewhere</a>
+                    </p>
+                  </div>
+                      
+                  <div class="row">
+                    <p>
+                      <div class="form-floating mb-3" style="width: 300px; margin-left: 70px;">
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Name" required>
+                        <label for="floatingInput">Capability</label>
                       </div>
-                    </div>
-                    </td>
-                  </tr>
-                </tbody>
-                
-              </table>
+                      <div class="form-floating mb-3" style="width: 300px;">
+                        <input type="text" class="form-control" name="number" id="number" placeholder="Number" required>
+                        <label for="floatingInput">Hour</label>
+                      </div>
+                    </p>
+                  </div>
+                  <div class="row">
+                    <p>
+                      <div class="form-floating mb-3" style="width: 300px; margin-left: 70px;">
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Name" required>
+                        <label for="floatingInput">Local</label>
+                      </div>
+                      <div class="form-floating mb-3" style="width: 300px;">
+                        <input type="file" class="form-control" name="number" id="number" placeholder="Number" required>
+                        <label for="floatingInput">Image</label>
+                      </div>
+                    </p>
+                  </div>
+              </div>
+          </form>
         </div>
     </div>
 </body>
-
 </html>
+
