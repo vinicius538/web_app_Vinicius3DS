b37c536d7923cb150873885c2edfcc483a758092
Vinicius de Souza <vinicius.souza110208@gmail.com>
2025-11-13 16:09:06 -0300

    Delete Web_App directory

--- Web_App/configuracoes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'web_app';
-    
-    $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-?>
